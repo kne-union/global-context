03f50d45b71855c927ecb7fd0f55a11be03c3adb
zhipenglin <sunandmoon001@163.com>
2024-11-12 13:48:04 +0800

    添加usePreset

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ export const { Provider, Consumer, useContext } = context;
 
 export const useGlobalContext = useContext;
 
+export const usePreset = () => {
+  const contextValue = useContext();
+  return Object.assign({}, { apis: {} }, contextValue?.preset);
+};
+
 export { createContext };
 
 export default context;
